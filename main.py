4cdab939b3e1fc172ae0278580cb4f0222235761
Danis Nurmansyah <danisnurman@users.noreply.github.com>
2024-07-11 17:44:03 +0700

    Update main.py

--- main.py
+++ main.py
@@ -41,15 +41,15 @@ X = df.drop(columns='Diabetes_binary')
 y = df.Diabetes_binary
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
-## Classifier
-# clf = DecisionTreeClassifier()
-# clf = clf.fit(X_train, y_train)
-# y_pred = clf.predict(X_test)
-
-# Classifier entropy criterion
-clf = DecisionTreeClassifier(criterion="entropy", splitter="best")
+# Classifier
+clf = DecisionTreeClassifier()
 clf = clf.fit(X_train, y_train)
 y_pred = clf.predict(X_test)
+
+# # Classifier entropy criterion
+# clf = DecisionTreeClassifier(criterion="entropy", splitter="best")
+# clf = clf.fit(X_train, y_train)
+# y_pred = clf.predict(X_test)
 
 # Evaluate the model
 streamlit.write("Accuracy: ", accuracy_score(y_test, y_pred))
