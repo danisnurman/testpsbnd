d5f933c7e30a6a68921e7ce0d73de064ba5350c5
Danis Nurmansyah <danisnurman@users.noreply.github.com>
2024-08-22 08:01:51 +0700

    Update main.py

--- main.py
+++ main.py
@@ -26,7 +26,9 @@ streamlit.title('Diabetes Health Indicators')
 
 ## Title
 streamlit.write("Hi! Please fill the form below.")
+##
 
+### BUILD MODEL WITH "ALL (21)" INDEPENDENT VARIABLE
 ## Read CSV & Define Feature
 df = pandas.read_csv('https://raw.githubusercontent.com/danisnurman/psbnd2/main/diabetes_binary_5050split_health_indicators_BRFSS2015.csv')
 # streamlit.dataframe(df, use_container_width=True)
@@ -121,46 +123,87 @@ def checkAgeCategory(age):
 checkAgeCategory(age)
 ## End of Age Categorization
 
-## Test the model
-bpVal = streamlit.number_input(label="High BP?", min_value=0, max_value=1)
-cholVal = streamlit.number_input(label="Cholesterol Total", min_value=10, max_value=500)
-bmiVal = streamlit.number_input(label="Body Mass Index", min_value=10, max_value=100)
-smokerVal = streamlit.number_input(label="Smoker?", min_value=0, max_value=1)
-physActVal = streamlit.number_input(label="Physical Activity?", min_value=0, max_value=1)
-new_data = [[bpVal, cholVal, bmiVal, smokerVal, physActVal]]
-result = clf.predict(new_data)
+# ## Predict New Data
+# new_data = [[bpVal, cholVal, bmiVal, smokerVal, physActVal]]
+# result = clf.predict(new_data)
 
-# ## Function to Check Health Status
-# # Chol Status
-# def checkCholStatus(cholVal):
-#     if(cholVal>=10 and cholVal<=200):
-#         cholStatus = "Normal"
-#     elif(cholVal>200):
-#         cholStatus = "High"
-#     return streamlit.write("Cholesterol status: ", cholStatus)
+# -------------------------------------------
 
-# # BMI Status
-# def checkBMIStatus2(bmiVal):
-#     if(bmiVal>=10 and bmiVal<18.5):
-#         bmiStatus = "Underweight"
-#     elif(bmiVal>=18.5 and bmiVal<=24.9):
-#         bmiStatus = "Normal"
-#     elif(bmiVal>=25 and bmiVal<=29.9):
-#         bmiStatus = "Overweight"
-#     elif(bmiVal>=30 and bmiVal<=34.9):
-#         bmiStatus = "Obese"
-#     elif(bmiVal>=35 and bmiVal<=100):
-#         bmiStatus = "Extremely Obese"
-#     return streamlit.write("BMI status: ", bmiStatus)
-# ## End of Function
+# ### BUILD MODEL WITH "5" INDEPENDENT VARIABLE
+# ## Read CSV & Define Feature
+# df = pandas.read_csv('https://raw.githubusercontent.com/danisnurman/psbnd2/main/diabetes_binary_5050split_health_indicators_BRFSS2015.csv')
+# # streamlit.dataframe(df, use_container_width=True)
+# df.dropna(inplace=True)
+# df.isnull().sum()
+# feature_cols = ['Diabetes_binary', 'HighBP', 'HighChol', 'BMI', 'Smoker', 'PhysActivity']
+# df = df[feature_cols]
+# streamlit.write(feature_cols)
+# ##
 
-# # Check Health Status
-# checkCholStatus(cholVal)
-# checkBMIStatus2(bmiVal)
+# ## Split the data
+# X = df.drop(columns='Diabetes_binary')
+# y = df.Diabetes_binary
+# X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+# ##
 
-## Show result
-# streamlit.write(bpVal, cholVal, bmiVal, smokerVal, physActVal)
-# streamlit.write(new_data)
+# ## Classifier
+# clf = DecisionTreeClassifier()
+# clf = clf.fit(X_train, y_train)
+# y_pred = clf.predict(X_test)
+
+# # Classifier entropy criterion
+# # clf = DecisionTreeClassifier(criterion="entropy", splitter="best")
+# # clf = clf.fit(X_train, y_train)
+# # y_pred = clf.predict(X_test)
+# ##
+
+# ## Evaluate the model
+# streamlit.write("Accuracy: ", accuracy_score(y_test, y_pred))
+# # streamlit.write(classification_report(y_test, y_pred))
+# ##
+
+# ## Test the model from Input
+# # bpVal = streamlit.number_input(label="High BP?", min_value=0, max_value=1)
+# # cholVal = streamlit.number_input(label="Cholesterol Total", min_value=10, max_value=500)
+# # bmiVal = streamlit.number_input(label="Body Mass Index", min_value=10, max_value=100)
+# # smokerVal = streamlit.number_input(label="Smoker?", min_value=0, max_value=1)
+# # physActVal = streamlit.number_input(label="Physical Activity?", min_value=0, max_value=1)
+
+# # ## Predict New Data
+# # new_data = [[bpVal, cholVal, bmiVal, smokerVal, physActVal]]
+# # result = clf.predict(new_data)
+
+# # ## Function to Check Health Status
+# # # Chol Status
+# # def checkCholStatus(cholVal):
+# #     if(cholVal>=10 and cholVal<=200):
+# #         cholStatus = "Normal"
+# #     elif(cholVal>200):
+# #         cholStatus = "High"
+# #     return streamlit.write("Cholesterol status: ", cholStatus)
+
+# # # BMI Status
+# # def checkBMIStatus2(bmiVal):
+# #     if(bmiVal>=10 and bmiVal<18.5):
+# #         bmiStatus = "Underweight"
+# #     elif(bmiVal>=18.5 and bmiVal<=24.9):
+# #         bmiStatus = "Normal"
+# #     elif(bmiVal>=25 and bmiVal<=29.9):
+# #         bmiStatus = "Overweight"
+# #     elif(bmiVal>=30 and bmiVal<=34.9):
+# #         bmiStatus = "Obese"
+# #     elif(bmiVal>=35 and bmiVal<=100):
+# #         bmiStatus = "Extremely Obese"
+# #     return streamlit.write("BMI status: ", bmiStatus)
+# # ## End of Function
+
+# # # Check Health Status
+# # checkCholStatus(cholVal)
+# # checkBMIStatus2(bmiVal)
+
+# ## Show result
+# # streamlit.write(bpVal, cholVal, bmiVal, smokerVal, physActVal)
+# # streamlit.write(new_data)
 
 ## Check Diabetes Risk
 if(result==0):
