0e75c365d6c2a750ac6605882a660da607f9c7f1
Danis Nurmansyah <danisnurman@users.noreply.github.com>
2024-08-24 19:39:34 +0700

    Update main.py

--- main.py
+++ main.py
@@ -46,6 +46,8 @@ X = df.drop(columns='Diabetes_binary')
 y = df.Diabetes_binary
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
+streamlit.write(X)
+
 ## Classifier
 clf = DecisionTreeClassifier()
 clf = clf.fit(X_train, y_train)
